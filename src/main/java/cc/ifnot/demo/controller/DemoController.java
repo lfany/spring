6d8a7e7a389c6d8840c1d0118abc2d72e8801716
hello <hello@ifnot.cc>
2017-12-22 15:48:19 +0800

    demo controller

---
+++ src/main/java/cc/ifnot/demo/controller/DemoController.java
@@ -0,0 +1,41 @@
+package cc.ifnot.demo.controller;
+
+import cc.ifnot.demo.bean.Base;
+import cc.ifnot.demo.bean.Demo;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping
+    public Base all() {
+        return new Base();
+    }
+
+    // @RequestParam 简单类型的绑定，可以出来get和post
+    @RequestMapping("/get")
+    public HashMap<String, Object> get(@RequestParam(value = "name", defaultValue = "aa") String name) {
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("title", "hello world!");
+        hashMap.put("name", name);
+        return hashMap;
+    }
+
+    // @PathVariable 获得请求url中的动态参数
+    @RequestMapping(value = "/get/{id}/{name}")
+    public Demo getDemo(@PathVariable(value = "id", required = false) int id, @PathVariable(value = "name", required = false) String name) {
+        Demo demo = new Demo();
+        demo.setId(id);
+        demo.setName(name);
+        demo.setDate(new Date());
+        return demo;
+    }
+
+}
