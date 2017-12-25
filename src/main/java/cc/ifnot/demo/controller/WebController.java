e13942ec6850154fb80740b9c4ed405b1b71f25d
hello <hello@ifnot.cc>
2017-12-25 13:36:26 +0800

    FreeMarker && Thymeleaf

--- src/main/java/cc/ifnot/demo/controller/WebController.java
+++ src/main/java/cc/ifnot/demo/controller/WebController.java
@@ -2,21 +2,30 @@ package cc.ifnot.demo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@EnableAutoConfiguration
 @RequestMapping("/web")
 public class WebController {
 
     private Logger logger = LoggerFactory.getLogger(WebController.class);
 
-    @RequestMapping("index")
-    public String index(ModelMap map) {
-        logger.info("info: web controller");
-        map.put("title", "hello world!");
-        return "index";
+    @RequestMapping("freemarker")
+    public String freemarker(ModelMap map) {
+        logger.info("info: web controller freemarker");
+        map.put("title", "freemarker!");
+        return "freemarker/index";
+    }
+
+    @RequestMapping("thymeleaf")
+    public String thymeleaf(ModelMap map) {
+        logger.info("info: web controller thymeleaf");
+        map.put("title", "thymeleaf!");
+        return "thymeleaf/index";
     }
 
 }
